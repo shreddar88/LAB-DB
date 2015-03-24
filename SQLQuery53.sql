8aca4aed2d853c09071ba0f837e971e16fc22bd3
Asgeir Sverrisson <asgeir_Sverrisson@hotmail.com>
2015-03-24 13:38:22 +0100

    LAB 3 not finished

--- SQLQuery53.sql
+++ SQLQuery53.sql
@@ -13,7 +13,7 @@ CREATE TABLE Author
 	YearOfDeath date NULL,
 	[Language] varchar(50) NOT NULL,
 	Country varchar(50) NOT NULL,
-	CONSTRAINT PK_Author_AuthorID PRIMARY KEY (AuthorID)
+		CONSTRAINT PK_Author_AuthorID PRIMARY KEY (AuthorID)
 );
 
 IF NOT EXISTS (SELECT object_id, type FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.Customer') AND type IN (N'U'))
@@ -26,7 +26,7 @@ CREATE TABLE Customer
 	Email varchar(50) NOT NULL,
 	Gender varchar(50) NOT NULL,
 	BirthYear date NOT NULL,
-	CONSTRAINT PK_Customer_CustomerID PRIMARY KEY(CustomerID),
+		CONSTRAINT PK_Customer_CustomerID PRIMARY KEY(CustomerID),
 );
 
 IF NOT EXISTS (SELECT object_id, type FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.Book') AND type IN (N'U'))
@@ -36,10 +36,9 @@ CREATE TABLE Book
 	AuthorID int NOT NULL,
 	Name varchar(50) NOT NULL,
 	PublishedYear date NOT NULL,
-	YearOfDeath datetime NULL,
 	[Language] varchar(50) NOT NULL,
-	CONSTRAINT PK_Book_BookID PRIMARY KEY(BookID),
-	CONSTRAINT FK_Book_AuthorID FOREIGN KEY(AuthorID) REFERENCES Author(AuthorID)
+		CONSTRAINT PK_Book_BookID PRIMARY KEY(BookID),
+		CONSTRAINT FK_Book_AuthorID FOREIGN KEY(AuthorID) REFERENCES Author(AuthorID)
 );
 
 IF NOT EXISTS (SELECT object_id, type FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.Status') AND type IN (N'U'))
@@ -47,7 +46,7 @@ CREATE TABLE [Status]
 (
 	StatusID int NOT NULL,
 	Value varchar(50) NOT NULL
-	CONSTRAINT PK_Status_StutusID PRIMARY KEY(StatusID)
+		CONSTRAINT PK_Status_StatusID PRIMARY KEY(StatusID)
 );
 
 IF NOT EXISTS (SELECT object_id, type FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.Copy') AND type IN (N'U'))
@@ -59,9 +58,9 @@ CREATE TABLE [Copy]
 	BookID int NOT NULL,
 	PurchaseCost money NULL,
 	PurchaseYear date NOT NULL
-	CONSTRAINT PK_Copy_CopyID PRIMARY KEY(CopyID)
-	CONSTRAINT FK_Copy_StatusID FOREIGN KEY(StatusID) REFERENCES [Status] (StatusID),
-	CONSTRAINT FK_Copy_BookID FOREIGN KEY (BookID) REFERENCES Book (BookID)
+		CONSTRAINT PK_Copy_CopyID PRIMARY KEY(CopyID)
+		CONSTRAINT FK_Copy_StatusID FOREIGN KEY(StatusID) REFERENCES [Status] (StatusID),
+		CONSTRAINT FK_Copy_BookID FOREIGN KEY (BookID) REFERENCES Book (BookID)
 );
 
 IF NOT EXISTS (SELECT object_id, type FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.Loan') AND type IN (N'U'))
@@ -70,50 +69,80 @@ CREATE TABLE Loan
 	LoanID int NOT NULL IDENTITY(1,1),
 	CustomerID int NOT NULL,
 	CopyID int NOT NULL,
-	LoanDate datetime NOT NULL,
-	ReturnDate datetime NOT NULL,
-	CONSTRAINT PK_Loan_LoanID PRIMARY KEY(LoanID),
-	CONSTRAINT FK_Loan_CustomerID FOREIGN KEY(CustomerID) REFERENCES Customer (CustomerID),
-	CONSTRAINT FK_Loan_CopyID FOREIGN KEY(CopyID) REFERENCES Copy (CopyID)
+	LoanDate datetime NULL
+		CONSTRAINT DF_Loan_LoanDate DEFAULT GetDate(),
+	ReturnDate datetime NULL
+		CONSTRAINT DF_Loan_ReturnDate DEFAULT DATEADD(DAY,14,GetDate()),
+		CONSTRAINT PK_Loan_LoanID PRIMARY KEY(LoanID),
+		CONSTRAINT FK_Loan_CustomerID FOREIGN KEY(CustomerID) REFERENCES Customer (CustomerID),
+		CONSTRAINT FK_Loan_CopyID FOREIGN KEY(CopyID) REFERENCES Copy (CopyID)	
 );
 
 DECLARE @StatusID int
+	, @LoanID int
+	, @AuthorID int
+	, @CustomerID int
+	, @BookID int
+	, @CopyID int
 	INSERT INTO dbo.[Status] ([StatusID], [Value])
 		VALUES (0, 'Available'),
 				 (1, 'Rented Out'),
 				 (2, 'Delayed')
 SET @StatusID = SCOPE_IDENTITY();
 
-
-DECLARE @AuthorID int
 	INSERT INTO dbo.Author (FirstName,LastName,YearOfBirth,YearOfDeath,[Language],[Country]) 
-		VALUES('Mattias','Thander','1957','2015', 'French', 'Sweden'); 
+		VALUES('Mattias','Thander','1957', NULL, 'French', 'Sweden')
+		
 SET @AuthorID = SCOPE_IDENTITY();
 
-DECLARE @CustomerID int
-	INSERT INTO dbo.Customer (FirstName, LastName, TelephoneNumber, Email, Gender, BirthYear)
-		VALUES ('Gedoc', 'Sintset', '0722636431', 'as@hotmail.com', 'Male', '1988')
-SET @CustomerID = SCOPE_IDENTITY();
-
-DECLARE @BookID int
-	INSERT INTO dbo.Book(AuthorID, Name, PublishedYear, YearOfDeath, [Language])
-		VALUES (@AuthorID, 'Alkemisten', '1989', '2029', 'Swedish')
+	INSERT INTO dbo.Book(AuthorID, Name, PublishedYear, [Language])
+		VALUES (@AuthorID, 'Alkemisten', '1989', 'Swedish')
+			
 SET @BookID = SCOPE_IDENTITY(); 
 
-
-
-DECLARE @CopyID int
-	INSERT INTO dbo.[Copy] (StatusID, BookID ,PurchaseCost,PurchaseYear) 
+	INSERT INTO dbo.Copy (StatusID, BookID ,PurchaseCost,PurchaseYear) 
 		VALUES (0, @BookID, '5000', '2007') 
+			
 SET @CopyID = SCOPE_IDENTITY();
 
-DECLARE @LoanID int
-	INSERT INTO dbo.Loan (CustomerID, CopyID, LoanDate, ReturnDate)
-		VALUES (@CustomerID, @CopyID, '2015-03-10','2015-03-24')
+INSERT INTO dbo.Customer (FirstName, LastName, TelephoneNumber, Email, Gender, BirthYear)
+		VALUES ('Gedoc', 'Sintset', '0722636431', 'as@hotmail.com', 'Male', '1988')
+
+SET @CustomerID = SCOPE_IDENTITY();
+
+	INSERT INTO dbo.Loan (CustomerID, CopyID)
+		VALUES (@CustomerID, @CopyID)
+		
+SET @LoanID = SCOPE_IDENTITY();
+-----------------------------------------
+	INSERT INTO dbo.Author (FirstName,LastName,YearOfBirth,YearOfDeath,[Language],[Country]) 
+		VALUES('Göran','Persson','1927', NULL, 'Swedish', 'German')
+		
+SET @AuthorID = SCOPE_IDENTITY();
+
+INSERT INTO dbo.Book(AuthorID, Name, PublishedYear, [Language])
+		VALUES (@AuthorID, 'Politik', '1999', 'Swedish')
+			
+SET @BookID = SCOPE_IDENTITY(); 
+
+	INSERT INTO dbo.Copy (StatusID, BookID ,PurchaseCost,PurchaseYear) 
+		VALUES (0, @BookID, '200', '2004') 
+			
+SET @CopyID = SCOPE_IDENTITY();
+
+INSERT INTO dbo.Loan (CustomerID, CopyID)
+		VALUES (@CustomerID, @CopyID)
+		
 SET @LoanID = SCOPE_IDENTITY();
 
-SELECT *
+
+SELECT * FROM Author
+SELECT * FROM Book
+SELECT * FROM Copy
+SELECT * FROM Loan
+SELECT * FROM Customer
 	
+SELECT *
 FROM Author AS A
 	INNER JOIN Book AS B ON B.AuthorID = A.AuthorID 
 	INNER JOIN Copy AS CO ON B.BookID = CO.BookID
@@ -122,6 +151,25 @@ FROM Author AS A
 	INNER JOIN [Status] AS S ON S.StatusID = CO.StatusID
 
 
+--sätt alla lånade böcker till status rented out
+UPDATE dbo.Copy
+SET StatusID = 1
+FROM dbo.Copy AS C
+	INNER JOIN dbo.Loan AS L ON L.CopyID = C.CopyID
+WHERE L.LoanDate IS NOT NULL
+
+---- View med vilka kunder som har lånade böcker
+--create view dbo.vCustomersWithBooks
+--as
+--select C.FirstName + ' ' + C.LastName as CustomerName
+--, L.Title
+--, cast(LL.Returndate as date) as 'ReturnDate'
+--from dbo.Customers as C
+--	inner join dbo.LitteratureLoan as LL on LL.CustomerID = C.ID
+--	inner join dbo.Litterature as L on L.ID = LL.LitteratureCopyID
+--where LL.Returndate > GETDATE();
+
 --USE master
 --DROP DATABASE MyLibrary;
 --CREATE DATABASE MyLibrary;
+--USE MyLibrary
